0ac5e237e34bb6d97ff422aad0e93f941f301627
Light <benzycassian@gmail.com>
2026-05-12 14:52:30 -0300

    Fix deployment update

--- src/api/warriorAiApi.js
+++
@@ -1,7 +0,0 @@
-import { httpRequest } from "./httpClient";
-
-export const createWarriorAiReplyRequest = (payload) =>
-  httpRequest("/api/warrior-ai", {
-    method: "POST",
-    body: JSON.stringify(payload),
-  });
